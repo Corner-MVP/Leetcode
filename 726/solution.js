72235cb09c16e087a788171fabbcb78babc9c05d
Stevenmata <1033165144@qq.com>
2021-07-06 07:33:41 +0800

    726

--- 726/solution.js
+++ 726/solution.js
@@ -1,63 +1,60 @@
 var countOfAtoms = function(formula) {
-  
-  let index = 0
-  const len = formula.length
-  const stack = [new Map()]
+  let i = 0;
+  const n = formula.length;
 
-  while (index < len) {
-    const ch = formula[index]
+  const stack = [new Map()];
+  while (i < n) {
+      const ch = formula[i];
 
-    const getAtom = () => {
-      const res = []
-      res.push(formula[index])
-      while (index < len && formula[index] >= 'a' && formula[index] <= 'z') {
-        res.push(formula[index])
-        index += 1
+      const parseAtom = () => {
+          const sb = [];
+          sb.push(formula[i++]); // 扫描首字母
+          while (i < n && formula[i] >= 'a' && formula[i] <= 'z') {
+              sb.push(formula[i++]); // 扫描首字母后的小写字母
+          }
+          return sb.join('');
       }
-      return res.join('')
-    }
 
-    const getNum = () => {
-      if (index === len || isNan(Number(formula[index]))) return 1
-
-      let num = 0
-      while (index < len && !isNan(Number(formula[index]))) {
-        num = num * 10 + formula[index].charCodeAt() - '0'.charCodeAt();
-        index += 1
+      const parseNum = () => {
+          if (i === n || isNaN(Number(formula[i]))) {
+              return 1; // 不是数字，视作 1
+          }
+          let num = 0;
+          while (i < n && !isNaN(Number(formula[i]))) {
+              num = num * 10 + formula[i++].charCodeAt() - '0'.charCodeAt(); // 扫描数字
+          }
+          return num;
       }
-    }
 
-    if (ch === '(') {
-      index += 1;
-      stack.unshift(new Map());
-    } else if (ch === ')') {
-      index += 1
-      const num = getNum()
-      popMap = stack.shift()
-      topMap = stack[0]
-      for (const [key, value] of popMap.entries()) {
-        topMap.set(key, topMap[key] || 0 + value * num)
+      if (ch === '(') {
+          i++;
+          stack.unshift(new Map()); // 将一个空的哈希表压入栈中，准备统计括号内的原子数量
+      } else if (ch === ')') {
+          i++;
+          const num = parseNum(); // 括号右侧数字
+          const popMap = stack.shift(); // 弹出括号内的原子数量
+          const topMap = stack[0];
+          for (const [atom, v] of popMap.entries()) {
+              topMap.set(atom, (topMap.get(atom) || 0) + v * num); // 将括号内的原子数量乘上 num，加到上一层的原子数量中
+          }
+      } else {
+          const atom = parseAtom();
+          const num = parseNum();
+          const topMap = stack[0];
+          topMap.set(atom, (topMap.get(atom) || 0) + num); // 统计原子数量
+          
       }
-    } else {
-      const atom = getAtom()
-      const num = getNum()
-      const topMap = stack[0]
-      topMap.set(atom, topMap[atom] || 0 + num)
-    }
-
   }
 
-  let map = stack.pop()
-  map = Array.form(map)
-  map.sort()
-  const res = []
-  for (const [key, value] of map) {
-    res.push(key)
-    if (value > 1) {
-      res.push(value)
-    }
+  let map = stack.pop();
+  map = Array.from(map);
+  map.sort();
+  const sb = [];
+  for (const [atom, count] of map) {
+      sb.push(atom);
+      if (count > 1) {
+          sb.push(count);
+      }
   }
-
-  return res.join('')
-
-}
+  return sb.join('');
+};
